5c4037723f27a72a511f325465d2b9ff2ab37e7f
juansanchesz <juan.vic@hotmail.com.br>
2026-08-24 22:56:53 -0300

    fiz o exercicio 1.5

--- exercicios/curso1_semana1/exercicio_1.5.js
+++ exercicios/curso1_semana1/exercicio_1.5.js
@@ -1,8 +1,6 @@
-
-
 const calcularDano = (ataque, defesa) => {
-    if (ataque < defesa) {
-        return 0;
-    }
+    if (ataque < defesa) return 0;
     return ataque - defesa;
 };
+console.log(calcularDano(50, 20)); // 30
+console.log(calcularDano(20, 50)); // 0
