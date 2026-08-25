ca7e72d5b6eecc7d78d54feee07265370fa00e27
juansanchesz <juan.vic@hotmail.com.br>
2026-08-24 21:40:40 -0300

    movi os exercicios de lugar, agora eles ficam na pasta correta, e também fiz o exercício 1.6.1

---
+++ exercicios/curso1_semana1/exercicio_1.6.1.js
@@ -0,0 +1,4 @@
+const nomeJogador = "Kael"; // coloquei const porque o nome não muda nunca
+const VIDA_MAXIMA = 100; // coloquei const porque a vida também não muda e por estar em maiúsculo é uma convenção para indicar que é uma constante
+let pontos = 0; // coloquei let porque os pontos são alterados logo na linha abaixo
+pontos += 10;
