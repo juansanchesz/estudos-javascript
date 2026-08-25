8a051a7cdc56af115000f9d8531be457c4d5a407
juansanchesz <juan.vic@hotmail.com.br>
2026-08-24 23:18:33 -0300

    fiz o exercício 1.6.2

--- exercicios/curso1_semana1/exercicio_1.6.2.js
+++ exercicios/curso1_semana1/exercicio_1.6.2.js
@@ -1,3 +1,17 @@
-let valor = 42;
+const descreverTipo = (valor) => {
+    const tipo = typeof valor;
 
-descreverTipo = (valor) => { return typeof valor; };
+    if (tipo !== "object") {
+        return `${tipo} - primitivo`;
+    } else if (valor === null) {
+        return `${tipo} - primitivo`;
+    } else {
+        return `${tipo} - object`;
+    }
+};
+
+console.log(descreverTipo(42));
+console.log(descreverTipo("oi"));
+console.log(descreverTipo(null));
+console.log(descreverTipo([1, 2]));
+console.log(descreverTipo({ a: 1 }));
